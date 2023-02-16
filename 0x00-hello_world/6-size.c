b2adfce0d2516301616eaee3a45046247f1fcffe
ELIXIR0765 <emmainede607@gmail.com>
2023-02-16 13:55:00 -0800

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int: %lu byt(s)\n", (unsigned long)sizeof(c));
-printf("size of long long: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a long int: %lu byt(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
