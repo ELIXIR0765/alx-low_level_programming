214f42583debd181abaa21354612d07a4d6a1906
ELIXIR0765 <emmainede607@gmail.com>
2023-02-22 14:37:12 -0800

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,19 +16,19 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+		printf("%d", i);
+		if (i != 98)
+		printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n < 0)
-	 {
+	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+		printf("%d", i);
+		if (i != 98)
+		printf(", ");
 		}
 		printf("\n");
 	}
@@ -36,9 +36,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+		printf("%d", i);
+		if (i != 98)
+		printf(", ");
 		}
 		printf("\n");
 	}
