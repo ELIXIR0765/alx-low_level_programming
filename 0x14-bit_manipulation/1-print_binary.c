69cf047c3f7b12e25acba7e7bfc49d2b44bcb8eb
ELIXIR0765 <emmainede607@gmail.com>
2023-04-16 01:07:29 -0700

    correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary - prints the binary equivalent of a decimal number
- * @b: number to print in binary
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
